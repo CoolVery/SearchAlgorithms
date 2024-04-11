3819e79aaab25ab7df0b272f4f7c8f9a79cad57b
GeorgyKA31P <gorgn2005@gmail.com>
2024-04-11 22:37:24 +0300

    Realize binary search algorithm

---
+++ SearchAlgorithms/BinarySearch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithms
+{
+    internal class BinarySearch
+    {
+        int[] array;
+        public BinarySearch(int length, int minValue, int maxValue)
+        {
+            array = new int[length];
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(minValue, maxValue);
+                
+            }
+            Array.Sort(array);
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($" {array[i]} ");
+            }
+        }
+        private int[] SearcElementBinary(int searchValue)
+        {
+            int start = 0;
+            int end = array.Length - 1;
+            int midArray;
+            while(start < end)
+            {
+                midArray = start + end;
+                int tempValue = array[midArray];
+                if (tempValue == searchValue)
+                {
+                    return new int[] {tempValue, midArray};
+                }
+                else if (tempValue > searchValue)
+                {
+                    end -= 1;
+                }
+                else if (tempValue < searchValue)
+                {
+                    start += 1;
+                }
+            }
+            return null;
+        }
+        public void GetAnver(int searchValue)
+        {
+            int[] answer = SearcElementBinary(searchValue);
+            if (answer == null)
+            {
+                Console.WriteLine("Элемента нет");
+            }
+            else Console.WriteLine($"Элемент - {answer[0]}\nИндекс - {answer[1]}");
+        }
+    }
+}
---
+++ SearchAlgorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearch binarySearch = new BinarySearch(20, 1, 25);
+            binarySearch.GetAnver(5);
+        }
+    }
+}
